39fc3e03fa95348ab6322a5ebbc1684e94e261f5
Richard Chung <ric_chung@yahoo.com>
2021-08-25 18:53:54 +1000

    Numbers

---
+++ Workshop4.py
@@ -0,0 +1,33 @@
+# Task 1
+print("I will now count my chickens:")
+print("Hens", 25 + 30 / 6) # 30.0
+print("Roosters", 100 - 25 * 3 % 4) # 97
+
+# print("Now should I count my eggs?") #does not print as this is a comment
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # 6.75
+
+print("Is it true that 13 ** 2 = 169?")
+
+print(13 ** 2) # 169
+
+print("What is 3 + 2?", 3 + 2) # 5
+
+print("What is 5 - 7?", 5 - 7) # -2
+
+# Task 2
+length = input("Enter length of cube: ")
+
+length = int(length)
+area = 6 * length ** 2
+
+print("Length of cube: ", length)
+print("The surface area of the cube is ", area)
+
+# Task 3
+light_speed = 3e8
+
+light_year = 60 * 60 * 24 * 365 * light_speed
+
+print(light_year)
