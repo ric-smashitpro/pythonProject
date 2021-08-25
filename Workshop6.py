94cf7a06bee293f5fc347e69ec91e43e5cc7e2bd
Richard Chung <ric_chung@yahoo.com>
2021-08-25 18:56:55 +1000

    Lists and Sets

---
+++ Workshop6.py
@@ -0,0 +1,50 @@
+# Task 1
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in the_count:
+    print(f"This is count {number}")
+
+# same as above
+for fruit in fruits:
+    print(f"A fruit of type: {fruit}")
+
+# also we can go through mixed lists too
+for i in change:
+    print(f"I got {i}")
+
+# we can also build lists, first start with an empty one
+elements = []
+
+# then use the range function to generate a list
+elements = range(0, 6)
+
+# now we can print them out too
+for i in elements:
+    print(f"Element was: {i}")
+
+# Task 2
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait there's not 10 things in that list, let's fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print("Adding: ", next_one)
+    stuff.append(next_one)
+    print(f"There's {len(stuff)} items now.")
+
+print("There we go: ", stuff)
+
+print("Let's do some things with stuff.")
+
+print(stuff[1])
+print(stuff[-1])  # whoa! fancy
+print(stuff.pop())
+print(' '.join(stuff))  # what? cool!
+print('#'.join(stuff[3:5]))  # super stellar!
