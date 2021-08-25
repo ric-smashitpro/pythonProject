2bdb3d8651b31626c8fe0c4b16a71821ff0927bf
Richard Chung <ric_chung@yahoo.com>
2021-08-25 18:43:50 +1000

    main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hello World")
+print("Naughty Pandas")
+
+
+s = "this IS a string"
+print(s.split(" "))
+print(s.count("is"))
+print(s.startswith("this"))
+print("string" in s)
+
+name = input("What is your name? ")
+print("My name is " + name)
+
+first_name = input("Your first name: ")
+last_name = input("Your last name: ")
+print(first_name.lower()+last_name.lower())
+
+# to comment a line press Ctrl + /
