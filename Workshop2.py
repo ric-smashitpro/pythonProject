4ebb20575c281d49123feb32dc18c703a32dcdf9
Richard Chung <ric_chung@yahoo.com>
2021-08-25 18:52:36 +1000

    Strings

---
+++ Workshop2.py
@@ -0,0 +1,58 @@
+print("15" + "20")
+print("My" + " name")
+print("My" + " " + "name")
+print(len("Give me a string"))
+print("Give me a string".title())
+
+
+x = 'There are 10 types of people.'
+binary = "binary"
+do_not = "don't"
+y = "Those who know " + binary + " and those who " + do_not + "."
+z = input("Can you say something? ")
+
+print(x)
+print(y)
+
+print("I said: " + x)
+print("I also said: " + y)
+print("You said: ", z)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
+
+
+print("Mary had a little lamb.")
+print("Its fleece was white as " + 'snow')
+print("And everywhere that Mary went.")
+print("." * 10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+print("Here are the days: " + days)
+print("Here are the months: " + months)
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
