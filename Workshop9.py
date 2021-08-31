8e53e24f0d103ca09ae9e3bbb07efd25c59bf8a7
Richard Chung <ric_chung@yahoo.com>
2021-08-31 17:58:24 +1000

    Files, CSV and JSON + String Formatting

--- Workshop9.py
+++ Workshop9.py
@@ -1,4 +1,6 @@
+##################
 # Workshop 9-part1
+##################
 # Task 1
 txt_file = open("ex9_sample.txt")
 try:
@@ -9,10 +11,22 @@ finally:
     txt_file.close()
 
 # Task 2
+# 1. using file open() format, try, for loop, finally file.close()
+f1 = open("ex9_sample.txt", "r")
+f2 = open("ex9_copy.txt", "w")
+try:
+    for line in f1:
+        f2.write(line)
+finally:
+    f1.close()
+    f2.close()
+
+# 2. using open() as file
 with open("ex9_sample.txt") as f1:
     with open("ex9_copy.txt", "w") as f2:
         for line in f1:
             f2.write(line)
+
 
 # Task 3
 import csv
@@ -31,6 +45,24 @@ finally:
 
 # Task 4
 # import packages to read csv file and write to json file
+# 2. Using file open(), try, for loop, finally file.close()
+import csv, json
+
+data = {}
+csvFile = open("ex9_cities.csv", "r")
+jsonFile = open("ex9_output.json", "w")
+try:
+    csv_reader = csv.DictReader(csvFile)
+    for rows in csv_reader:
+        record = rows["rank"]
+        data[record] = rows
+
+    jsonFile.write(json.dumps(data))
+finally:
+    csvFile.close()
+    jsonFile.close()
+
+# 2. Using with open() as file
 import csv, json
 
 csvFilePath = "ex9_cities.csv"
@@ -46,25 +78,30 @@ with open(csvFilePath, "r") as csvFile:
 with open(jsonFilePath, "w") as jsonFile:
     jsonFile.write(json.dumps(data))
 
+
+##################
 # Workshop 9-part2
+##################
 import csv
 
 rank_header = "Rank"
 city_header = "City"
-pop_header= "Population"
+pop_header = "Population"
 
 file = open("ex9_cities.csv", "r")
 try:
     csv_reader = csv.DictReader(file)
+
     i = 0
     for row in csv_reader:
+        rank = int(row["rank"])
+        city = row["city"]
+        pop = int(row["population"])
         if i == 0:
             print(f"{rank_header:>4} {city_header:<20}{pop_header:>12}")
             print("=" * 37)
+            print(f"{rank:>4} {city:<20}{pop:>12,d}")
         else:
-            rank = int(row["rank"])
-            city = row["city"]
-            pop = int(row["population"])
             print(f"{rank:>4} {city:<20}{pop:>12,d}")
         i += 1
 finally:
